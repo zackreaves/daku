3b41b8c5d4542766dd374098d61737b6c653b499
Zack <zreaves@protonmail.com>
2023-02-21 00:53:49 -0800

    Added a mostly complete SQL script for initializing the database tables.

---
+++ src/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE players {
+  id SERIAL PRIMARY KEY,
+  namef VARCHAR(80),
+};
+
+CREATE TABLE games (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(80),
+  ties BOOLEAN,
+);
+
+CREATE TABLE round_data (
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES players(id),
+  round_count INTEGER, -- FIXME: Add some automated logic that sums the rounds based on player data.
+  ties INTEGER NULL,
+  player_count INTEGER,
+  -- FIXME: Add date and time.
+);
+
+CREATE TABLE player_data (
+  round_id INTEGER REFERENCES round_data(id),
+  player_id INTEGER REFERENCES players(id),
+  wins INTEGER,
+)
