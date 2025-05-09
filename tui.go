00b7e0e9937605d20bb045b3445b1b857308c088
Zackary Reaves <reaves.zackary@gmail.com>
2025-05-08 18:57:51 -0700

    Added validation for player_count input.

--- tui.go
+++ tui.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/charmbracelet/huh"
 )
 
 func Match_input_form() error {
 	var player_count string
-	player_count_input := huh.NewInput().Title("How Many Players?").Value(&player_count)
+	player_count_input := huh.NewInput().Title("How Many Players?").Value(&player_count).Validate(func (player_count string) error {
+			_,err := strconv.ParseUint(player_count,10,64)
+			if err != nil {
+				return fmt.Errorf("Requires Natural/Positive Whole Number.")
+			}
+
+			return nil
+	})
 
 	page_one := huh.NewGroup(player_count_input)
 
